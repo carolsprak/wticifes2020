ec24ede30325e579a4b035506b1ecc631f3c9555
Anne Caroline <carolsprak@gmail.com>
2020-03-24 17:55:42 -0300

    criacao layout de tela de programacao e salas

---
+++ lib/ui/agenda/agenda_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgendaScreen extends StatefulWidget {
+  @override
+  _AgendaScreenState createState() => _AgendaScreenState();
+}
+
+class _AgendaScreenState extends State<AgendaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/ui/atividade/atividade_mixin.dart
+++ lib/ui/atividade/atividade_mixin.dart
@@ -1,19 +1,127 @@
 import 'package:flutter/material.dart';
 
 mixin AtividadeMixin {
+   
+  Widget diaUm() {
 
-  Widget buildBody() {
     return StreamBuilder<bool>(
       initialData: false,
       builder: (context, snapshot) {
-        return Center(
-          child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text("Entrou"),
-                  ])
-        );
-      });
+        return DefaultTabController(
+      length: 3,
+      child: Scaffold(          
+        backgroundColor: Color(0xffE0E7EE),
+          body: TabBarView(            
+            children: [
+              salaUm(1),              
+              salaDois(1),
+              salaTres(1)
+              ],
+          ),
+          bottomNavigationBar: 
+            TabBar(
+              unselectedLabelColor: Color(0xff5A80B9),
+              indicatorColor: Color(0xffFF6600),
+              labelColor: Color(0xffFF6600),
+              tabs: [
+                Tooltip( message: "Sala Jacumã", child: Tab(text:  "Jacumã")),
+                Tooltip(message: "Sala Tabatinga", child: Tab(text: "Tabatinga")),
+                Tooltip(message: "Sala Tambaba", child: Tab(text: "Tambaba")),
+        ])
+        ));
+      }
+    );
+
+  }
+  
+  Widget diaDois() {
+
+    return StreamBuilder<bool>(
+      initialData: false,
+      builder: (context, snapshot) {
+        return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          backgroundColor: Color(0xffE0E7EE),
+          body: TabBarView(          
+            children: [
+              salaUm(2),              
+              salaDois(2)
+              ],
+          ),
+          bottomNavigationBar: 
+            TabBar(
+              unselectedLabelColor: Color(0xff5A80B9),
+              indicatorColor: Color(0xffFF6600),
+              labelColor: Color(0xffFF6600),
+              tabs: [
+                Tooltip( message: "Sala Jacumã", child: Tab(text:  "Jacumã")),
+                Tooltip(message: "Sala Tabatinga", child: Tab(text: "Tabatinga"))
+        ])
+        ));
+      }
+    );
+
+  }
+  
+  Widget diaTres() {
+
+    return StreamBuilder<bool>(
+      initialData: false,
+      builder: (context, snapshot) {
+        return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          backgroundColor: Color(0xffE0E7EE),
+          body: TabBarView(
+            children: [
+              salaUm(3),              
+              salaDois(3),              
+              ],
+          ),
+          bottomNavigationBar: 
+            TabBar(
+              unselectedLabelColor: Color(0xff5A80B9),
+              indicatorColor: Color(0xffFF6600),
+              labelColor: Color(0xffFF6600),
+              tabs: [
+                Tooltip( message: "Sala Jacumã", child: Tab(text:  "Jacumã")),
+                Tooltip(message: "Sala Tabatinga", child: Tab(text: "Tabatinga"))                
+        ])
+        ));
+      }
+    );
+
   }
 
+  Widget salaUm(int i) {
+    return StreamBuilder<bool>(
+      initialData: false,
+      builder: (context, snapshot) {
+        return Container(color: Colors.white,
+        child: Text('Sala 1', textAlign: TextAlign.center , style: TextStyle(fontSize: 20),),);
+      }
+    );
+
+  } 
+  
+  Widget salaDois(int i) {
+    return StreamBuilder<bool>(
+      initialData: false,
+      builder: (context, snapshot) {
+        return Container(color: Colors.white,
+        child: Text('Sala 2', textAlign: TextAlign.center , style: TextStyle(fontSize: 20),),);
+      }
+    );
+  } 
+
+  Widget salaTres(int i) {
+    return StreamBuilder<bool>(
+      initialData: false,
+      builder: (context, snapshot) {
+        return Container(color: Colors.white,
+        child: Text('Sala 3', textAlign: TextAlign.center , style: TextStyle(fontSize: 20),),);
+      }
+    );
+  } 
 }
